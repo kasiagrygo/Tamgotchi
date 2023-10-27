0bf2be5f5c7efbed8f40367bf9e01c88c20a497e
kasiagrygo <katarzynagrygo@icloud.com>
2023-10-27 03:37:26 +0200

    Dodaj pliki projektów.

---
+++ Tamgotchi/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Tamagotchi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Tamagotchi");
+            Console.WriteLine("Start");
+            Console.WriteLine("Koniec");
+        }
+    }
+}
