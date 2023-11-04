dcf1299d6c1c7433ad67ba536ddf014dfab698b4
kasiagrygo <katarzynagrygo@icloud.com>
2023-11-04 01:52:28 +0100

    comit

---
+++ Tamgotchi/FileName.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terminal.Gui;
+
+namespace Tamgotchi
+{
+    public class LoginWindow : Window
+    {
+        private readonly View _parent;
+        public Action<bool> OnLogin { get; set; }
+        public Action OnExit { get; set; }
+
+        public LoginWindow(View parent) : base("Login", 5)
+        {
+            _parent = parent;
+            InitControls();
+            InitStyle();
+        }
+
+        public void InitStyle()
+        {
+            X = Pos.Center();
+            Width = Dim.Percent(50);
+            Height = Dim.Percent(100);
+        }
+
+        public void Close()
+        {
+            Application.RequestStop();
+            _parent?.Remove(this);
+        }
+
+        private void InitControls()
+        {
+            var nameLabel = new Label(0, 0, "Nickname");
+            var nameText = new TextField("")
+            {
+                X = Pos.Left(nameLabel),
+                Y = Pos.Top(nameLabel) + 1,
+                Width = Dim.Fill()
+            };
+            Add(nameLabel);
+            Add(nameText);
+
+            var birthLabel = new Label("Birthday")
+            {
+                X = Pos.Left(nameText),
+                Y = Pos.Top(nameText) + 1
+            };
+            var birthText = new TextField("")
+            {
+                X = Pos.Left(birthLabel),
+                Y = Pos.Top(birthLabel) + 1,
+                Width = Dim.Fill()
+            };
+            Add(birthLabel);
+            Add(birthText);
+
+            var tokenLabel = new Label("Tokens")
+            {
+                X = Pos.Left(birthText),
+                Y = Pos.Top(birthText) + 1
+            };
+            var tokenText = new TextField("")
+            {
+                X = Pos.Left(tokenLabel),
+                Y = Pos.Top(tokenLabel) + 1,
+                Width = Dim.Fill()
+            };
+            Add(tokenLabel);
+            Add(tokenText);
+
+            var loginButton = new Button("Login", true)
+            {
+                X = Pos.Left(tokenText),
+                Y = Pos.Top(tokenText) + 1
+            };
+
+            var exitButton = new Button("Exit")
+            {
+                X = Pos.Right(loginButton) + 5,
+                Y = Pos.Top(loginButton)
+            };
+
+            Add(loginButton);
+            Add(exitButton);
+
+            loginButton.Clicked += () =>
+            {
+                if (nameText.Text.ToString().TrimStart().Length == 0)
+                {
+                    MessageBox.ErrorQuery(25, 8, "Error", "Name cannot be empty.", "Ok");
+                    return;
+                }
+
+                var isDateValid = DateTime.TryParse(birthText.Text.ToString(), out DateTime birthDate);
+
+                if (string.IsNullOrEmpty(birthText.Text.ToString()) || !isDateValid)
+                {
+                    MessageBox.ErrorQuery(25, 8, "Error", "Date is required\nor is invalid.", "Ok");
+                    return;
+                }
+
+                int Age = CalculateAgeCorrect(birthDate, DateTime.Now);
+
+                if (Age < 18)
+                {
+                    if (Age > 0)
+                    {
+                        MessageBox.ErrorQuery(25, 8, "Error", $"You have {Age} years. You are too young to play casino games.", "Ok");
+                        return;
+                    }
+                    else
+                    {
+                        MessageBox.ErrorQuery(25, 8, "Error", $"You have {Age} years. You weren't even borned yet.", "Ok");
+                        return;
+                    }
+
+                }
+                if (Age > 100)
+                {
+                    MessageBox.ErrorQuery(25, 8, "Error", $"You have {Age} years. It can't be possible, please enter your real age.", "Ok");
+                    return;
+                }
+
+                if (!double.TryParse(tokenText.Text.ToString(), out double value) || value <= 0)
+                {
+                    MessageBox.ErrorQuery(25, 8, "Error", "Token ammount is invalid.", "Ok");
+                    return;
+                }
+
+         
+                OnLogin?.Invoke(true);
+
+                Close();
+            };
+
+            exitButton.Clicked += () =>
+            {
+                OnExit?.Invoke();
+                Close();
+            };
+        }
+
+        private int CalculateAgeCorrect(DateTime birthDate, DateTime now)
+        {
+            int age = now.Year - birthDate.Year;
+
+            if (now.Month < birthDate.Month || (now.Month == birthDate.Month && now.Day < birthDate.Day))
+                age--;
+
+            return age;
+        }
+    }
+}
+
--- Tamgotchi/Program.cs
+++ Tamgotchi/Program.cs
@@ -1,14 +1,94 @@
 ﻿using System;
 using Tamgotchi;
+using Terminal.Gui;
 
 namespace Tamagotchi
 {
     internal class Program
     {
+        public static bool RollMode { get; set; }
+        public static bool Muted { get; set; }
+        public static Toplevel top { get; set; }
+        private static string muteString { get; set; }
+
         static void Main(string[] args)
         {
-           Game myGame = new Game();
-           myGame.Start();
+            Application.Init();
+            Colors.Base = Colors.ColorSchemes["Error"];
+
+            muteString = "Mute";
+
+            top = Application.Top;
+
+            Window mainWindow = new Window("KCK Casino")
+            {
+                X = 0,
+                Y = 1,
+                Width = Dim.Fill(),
+                Height = Dim.Fill()
+            };
+            FrameView frameView = new FrameView("Window 1.")
+            {
+                X = 0,
+                Y = 1,
+                Width = Dim.Percent(50),
+                Height = Dim.Fill()
+            };
+            FrameView frameView2 = new FrameView("Window 2.")
+            {
+                X = Pos.Right(frameView),
+                Y = 1,
+                Width = Dim.Percent(50),
+                Height = Dim.Fill()
+            };
+
+            mainWindow.Add(frameView);
+            mainWindow.Add(frameView2);
+
+            top.Add(CreateMenuBar());
+
+            LoginWindow loginWindow = new(null)
+            {
+                OnExit = () => Application.RequestStop(),
+
+                OnLogin = (loginData) =>
+                {
+                    // for thread-safety
+                    Application.MainLoop.Invoke(() =>
+                    {
+
+                        top.Add(mainWindow);
+                    });
+
+                    Application.Run(top);
+                },
+            };
+
+            Application.Run(loginWindow);
+
+
+            //Game myGame = new Game();
+            //myGame.Start();
+        }
+        private static MenuBar CreateMenuBar()
+        {
+            return new MenuBar(new MenuBarItem[]
+            {
+                new MenuBarItem("_App", new MenuItem[]
+                {
+                    new MenuItem("_Quit", "", () => Application.RequestStop())
+                }), // end of file menu
+
+                new MenuBarItem("_Help", new MenuItem[]
+                {
+                    new MenuItem("_About", "", ()
+                                => MessageBox.Query(50, 5, "About", "KCK Casino v0.1", "Ok")),
+                    new MenuItem("_Roulette rules", "", ()
+                                => MessageBox.Query(50, 10, "Rules", $"Earn your bet times {2} by getting two same symbols side by side, or {3} when all three symbols are the same.", "Ok")),
+                }), // end of the help menu
+            });
         }
     }
+
+
 }
